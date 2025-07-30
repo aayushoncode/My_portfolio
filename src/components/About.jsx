02686f5faa15ac6b08cfa62122a1058543030322
codeAyush05 <ayushh.dev05@gmail.com>
2025-07-30 10:42:25 +0530

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = ({ darkMode }) => {
       id="about"
       className={`w-full overflow-x-hidden py-12 md:py-20 px-4 sm:px-6 lg:px-8 xl:px-10 max-w-[100vw] ${
         darkMode ? "bg-gray-800" : "bg-indigo-50"
-      }`}   >
+      }`}>
       <div className="container mx-auto">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
